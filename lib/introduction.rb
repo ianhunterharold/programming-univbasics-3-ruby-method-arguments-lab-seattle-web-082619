792c59621698a11bd3ac285a923770ff2ea9eccd
Ian Harold <ianhunterharold@gmail.com>
2019-07-26 19:55:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,3 +12,5 @@ end
 def introduction_with_language(name,language = Ruby, language*)
 puts  "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
+
+introduction_with_language(Josh,Ruby,Linex)
