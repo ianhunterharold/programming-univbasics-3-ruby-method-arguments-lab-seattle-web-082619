ac27277c19f0b25445c7c697871db5669476317f
Ian Harold <ianhunterharold@gmail.com>
2019-07-26 20:05:49 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,5 +12,3 @@ end
 def introduction_with_language_optional(name,language = RubyVM)
 puts  "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
-
-introduction_with_language_optional(Josh, Lenux)
