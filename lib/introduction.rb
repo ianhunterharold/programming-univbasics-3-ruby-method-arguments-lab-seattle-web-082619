8a827820fc5d34b463c111020bf0ab330e00eb17
Ian Harold <ianhunterharold@gmail.com>
2019-07-26 19:34:50 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,11 @@
 #introduction
-introduction(name)
+def introduction(name)
 puts "Hi my name is #{name}."
 end 
-introduction(Josh)
+
+
+
+    #method name      #parameter
+#def greeting_a_person(name)
+#  "Hello #{name}"
+end
